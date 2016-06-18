b1e6d196ca3afc9c7ead763c11e7bb26ad51ddbb
Vishal Prabhu <vishal.prabhu@gmail.com>
2016-06-18 16:01:27 +0530

    Cosmetic. Added ETA estimation to ApplyCompensation.

--- ApplyCompensation.m
+++ ApplyCompensation.m
@@ -5,6 +5,7 @@ outLum = zeros(size(fnames));
 
 readfile = @(dir, f) imread(fullfile(dir, f));
 savefile = @(img, name) imwrite(img, fullfile(outFolder, name));
+eta = ETA(tic, imageCount);
 for f=1:imageCount
     img = double(readfile(inFolder , fnames{f}));
     imgComp = img.*exp(compensation(f));
@@ -13,7 +14,9 @@ for f=1:imageCount
     
     outLum(f) = CalcLuminosity(imgComp);
     savefile(uint8(imgComp), fnames{f});
-    fprintf('%i\n', f);
+    
+    
+    eta.update(); eta.print();
 end
 
 end
